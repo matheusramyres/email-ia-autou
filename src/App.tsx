c495f841e3d039f09e62eb548705c9dc05a1603e
Matheus Ramyres <matheus63360@gmail.com>
2026-01-30 17:51:55 -0300

    Revert "teste ci"

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-amber-500">
+      <div className="bg-amber-400">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
